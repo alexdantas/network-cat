0a8d07b1b26635fb25b72d6cb01baa99b203532b
Alexandre Dantas <eu@alexdantas.net>
2014-10-29 06:26:05 -0700

    Improved readability on "Client"
    - Changed variable names...
    - Commented a little...

--- src/Client.cpp
+++ src/Client.cpp
@@ -16,43 +16,53 @@
 #include <sys/types.h>
 #include <ifaddrs.h>
 
-Client::Client(std::string ip, int port):
-	port(port)
+Client::Client(std::string server_hostname, int port):
+	server_hostname(server_hostname),
+	server_port(port),
+	socket(NULL)
 {
 	this->socket = new UDPSocket();
 	Log::debug("Socket created");
 
-	this->host = Network::hostToIp(ip);
-	Log::verbose("Client created on " + ip +
-	             " (" + this->host + ")" +
+	this->server_ip = Network::hostToIp(server_hostname);
+	Log::verbose("Client created on " + this->server_hostname +
+	             " (" + this->server_ip + ")" +
 	             " : " + Log::intToString(port));
 }
 void Client::send(std::string msg)
 {
-	// ugly as fuark
-	struct sockaddr_in server_addr;
+	// Socket address to the server.
+	// This is the raw connection we actually make to the server.
+	// We send raw data through this.
+	struct sockaddr_in server_address;
+	memset(&server_address, '\0', sizeof(server_address));
 
-	memset(&server_addr, '\0', sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port   = htons(this->port);
+	// It's an IP connection on a specific port
+	server_address.sin_family = AF_INET;
+	server_address.sin_port   = htons(this->server_port);
 
-	struct hostent* my_host;
+	// To send stuff to the server we need to know about it.
+	// This is the information we get from the server
+	// (IP address, along with other useless stuff)
+	struct hostent* server_host_entity;
 
-	my_host = gethostbyname(this->host.c_str());
-	if (my_host == NULL)
+	// Retrieving it...
+	server_host_entity = gethostbyname(this->server_ip.c_str());
+	if (server_host_entity == NULL)
 		throw std::string(strerror(errno));
 
+	// Storing the server information on that connection thingy
+	memcpy((void*) &server_address.sin_addr,
+	       server_host_entity->h_addr_list[0],
+	       server_host_entity->h_length);
 
-	memcpy((void*) &server_addr.sin_addr,
-	       my_host->h_addr_list[0],
-	       my_host->h_length);
-
+	// Finally actually sending information to the server!
 	int ret = sendto(this->socket->raw_socket,
 	                 msg.c_str(),
 	                 msg.length(),
 	                 0, // flags
-	                 (struct sockaddr*) &server_addr,
-	                 sizeof(server_addr));
+	                 (struct sockaddr*) &server_address,
+	                 sizeof(server_address));
 
 	if (ret < 0)
 		throw std::string(strerror(errno));
--- src/Client.hpp
+++ src/Client.hpp
@@ -8,8 +8,12 @@
 class Client
 {
 public:
-	/// Creates a client that will send to `ip`/`port`.
-	Client(std::string ip, int port);
+	/// Creates a client with a socket to #server_hostname and #port.
+	///
+	/// It will translate #server_hostname to an ip and
+	/// send messages to "server_ip:port".
+	/// For instance, "localhost" will get translated to "127.0.0.1"..
+	Client(std::string server_hostname, int port);
 
 	/// Sends `msg` to a server specified when created.
 	void send(std::string msg);
@@ -21,9 +25,18 @@ public:
 	void run();
 
 private:
-	int port;
+	/// Original server hostname sent to the Client constructor.
+	/// It could be an IP address, if user provided one.
+	std::string server_hostname;
+
+	/// Translated IP address from #server_hostname.
+	/// For example, "localhost" to "127.0.0.1".
+	std::string server_ip;
+
+	/// Port to connect to the server.
+	int server_port;
+
 	UDPSocket* socket;
-	std::string  host;
 };
 
 #endif //CLIENT_H_DEFINED
